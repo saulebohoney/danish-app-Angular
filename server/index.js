1ec6fa31c5fe0f18d5c365b22512e394a195a569
Saule BN <saulebohoney@gmail.com>
2017-08-16 19:39:01 -0400

    wt oauth

--- server/index.js
+++ server/index.js
@@ -1,40 +1,40 @@
 const path = require('path');
 const express = require('express');
-const passport = require('passport');
-const GoogleStrategy = require('passport-google-oauth20').Strategy;
-const BearerStrategy = require('passport-http-bearer').Strategy;
+//const passport = require('passport');
+//const GoogleStrategy = require('passport-google-oauth20').Strategy;
+//const BearerStrategy = require('passport-http-bearer').Strategy;
 const mongoose = require('mongoose');
 const bodyParser = require ('body-parser');
 
 mongoose.Promise = global.Promise;
 
-require ('dotenv').config();
+//require ('dotenv').config();
 const {DATABASE_URL, PORT} = process.env;
 const {Question, User} = require('./models');
 
-let secret = {
-    CLIENT_ID: process.env.CLIENT_ID,
-    CLIENT_SECRET: process.env.CLIENT_SECRET
-};
-if(process.env.NODE_ENV != 'production') {
-    secret = require('./secret');
-}
+// let secret = {
+//     CLIENT_ID: process.env.CLIENT_ID,
+//     CLIENT_SECRET: process.env.CLIENT_SECRET
+// };
+// if(process.env.NODE_ENV != 'production') {
+//     secret = require('./secret');
+// }
 
 const app = express();
 
-app.use(passport.initialize());
+//app.use(passport.initialize());
 app.use(bodyParser.json());
 
-app.get('/api/users/:accessToken',  (req, res) => {
-    User
-    .findOne({accessToken: req.params.accessToken})
-    .then(user =>{
-        return res.json(user);
-    })
-        .catch(err => {
-        res.status(500).json({error: 'Something went wrong!!!'});
-    });
-});
+// app.get('/api/users/:accessToken',  (req, res) => {
+//     User
+//     .findOne({accessToken: req.params.accessToken})
+//     .then(user =>{
+//         return res.json(user);
+//     })
+//         .catch(err => {
+//         res.status(500).json({error: 'Something went wrong!!!'});
+//     });
+// });
 //Google Strategy
 // passport.use(
 //     new GoogleStrategy({
@@ -76,56 +76,59 @@ app.get('/api/users/:accessToken',  (req, res) => {
 // );
 
 //Google Auth
-app.get('/api/auth/google',
-    passport.authenticate('google', {scope: ['profile']}));
+// app.get('/api/auth/google',
+//     passport.authenticate('google', {scope: ['profile']}));
 
-app.get('/api/auth/google/callback',
-    passport.authenticate('google', {
-        failureRedirect: '/',
-        session: false
-    }),
-    (req, res) => {
-        res.cookie('accessToken', req.user.accessToken, {expires: 0});
-        res.redirect('/');
-    }
-);
+// app.get('/api/auth/google/callback',
+//     passport.authenticate('google', {
+//         failureRedirect: '/',
+//         session: false
+//     }),
+//     (req, res) => {
+//         res.cookie('accessToken', req.user.accessToken, {expires: 0});
+//         res.redirect('/');
+//     }
+// );
 
-app.get('/api/auth/logout', (req, res) => {
-    req.logout();
-    res.clearCookie('accessToken');
-    res.redirect('/');
-});
+// app.get('/api/auth/logout', (req, res) => {
+//     req.logout();
+//     res.clearCookie('accessToken');
+//     res.redirect('/');
+// });
 
-//Authentication
-app.get('/api/me',
-    passport.authenticate('bearer', {session: false}),
-    (req, res) => {
-        return res.json({
-            hello:'it worked!',
-            googleId: req.user.googleId
-        })
-    }
-);
+// //Authentication
+// app.get('/api/me',
+//     passport.authenticate('bearer', {session: false}),
+//     (req, res) => {
+//         return res.json({
+//             hello:'it worked!',
+//             googleId: req.user.googleId
+//         })
+//     }
+// );
 
 app.get('/api/questions',
     //passport.authenticate('bearer', {session: false}),
     (req, res) => {
         Question
         .find()
+        .exec()
         .then(questions =>{
-            return res.json(questions.map(question =>question.apiRepr()));
+            res.json(questions.map(Question => Question.apiRepr()));
+            //return res.json(questions.map(question =>question.apiRepr()));
         })
         .catch(err => {
+            console.log(err);
             res.status(500).json({error: 'Something went wrong!!!'});
         });
     });
 
-app.use(express.static(path.resolve(__dirname, '../client/build')));
+// app.use(express.static(path.resolve(__dirname, '../client/build')));
 
-app.get(/^(?!\/api(\/|$))/, (req, res) => {
-    const index = path.resolve(__dirname, '../client/build', 'index.html');
-    res.sendFile(index);
-});
+// app.get(/^(?!\/api(\/|$))/, (req, res) => {
+//     const index = path.resolve(__dirname, '../client/build', 'index.html');
+//     res.sendFile(index);
+// });
 
 let server;
 function runServer(port=3001) {
